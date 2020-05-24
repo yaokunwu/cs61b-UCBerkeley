2e6599354bb4c601ae51cb518ecc1b19e40080ae
Yaokun Wu <yaokun.wu.2017@gmail.com>
2020-05-24 04:04:43 -0500

    proj1a submission2

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,56 +1,55 @@
+
 public class ArrayDeque<T> {
     private int size;
     private int nextfirst;
     private int nextlast;
     private T[] items;
-    public ArrayDeque(){
+    public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
         nextfirst = 0;
         nextlast = 1;
     }
 
-    public void addFirst(T item){
-        if (nextfirst==nextlast){
-            T[] newitems = (T[]) new Object[items.length*2];
-            int back_length = items.length - nextfirst-1;
-            int back_position = newitems.length - back_length;
+    public void addFirst(T item) {
+        if (nextfirst == nextlast) {
+            T[] newitems = (T[]) new Object[items.length * 2];
+            int backLength = items.length - nextfirst - 1;
+            int backPosition = newitems.length - backLength;
             System.arraycopy(items, 0, newitems, 0, nextfirst);
-            System.arraycopy(items, nextfirst+1, newitems, back_position, back_length);
-            nextfirst = back_position - 1;
+            System.arraycopy(items, nextfirst + 1, newitems, backPosition, backLength);
+            nextfirst = backPosition - 1;
             items = newitems;
         }
 
-        items[nextfirst]=item;
+        items[nextfirst] = item;
         size = size + 1;
-        if (nextfirst==0) {
-            nextfirst = items.length-1;
-        }
-        else {
+        if (nextfirst == 0) {
+            nextfirst = items.length - 1;
+        } else {
             nextfirst = nextfirst - 1;
-      }
+        }
 
 
     }
-    public void addLast(T item){
-        if (nextfirst==nextlast){
-            T[] newitems =(T[]) new Object[items.length*2];
-            int back_length = items.length - nextfirst-1;
-            int back_position = newitems.length - back_length;
+    public void addLast(T item) {
+        if (nextfirst == nextlast) {
+            T[] newitems = (T[]) new Object[items.length * 2];
+            int backLength = items.length - nextfirst - 1;
+            int backPostion = newitems.length - backLength;
             System.arraycopy(items, 0, newitems, 0, nextfirst);
-            System.arraycopy(items, nextfirst+1, newitems, back_position, back_length);
-            nextfirst = back_position - 1;
+            System.arraycopy(items, nextfirst + 1, newitems, backPostion, backLength);
+            nextfirst = backPostion - 1;
             items = newitems;
         }
 
-       items[nextlast] = item;
-       size = size + 1;
-       if (nextlast==items.length-1){
-           nextlast = 0;
-       }
-       else{
-           nextlast = nextlast + 1;
-       }
+        items[nextlast] = item;
+        size = size + 1;
+        if (nextlast == items.length - 1) {
+            nextlast = 0;
+        } else {
+            nextlast = nextlast + 1;
+        }
     }
 
     public boolean isEmpty() {
@@ -63,92 +62,93 @@ public class ArrayDeque<T> {
 
     public void printDeque() {
         int start = nextfirst;
-        for (int i = 0; i < size ; i++){
+        for (int i = 0; i < size; i++) {
             if (start == items.length - 1) {
                 start = 0;
-            }
-            else{
+            } else {
                 start = start + 1;
             }
-            System.out.print(items[start]+" ");
+            System.out.print(items[start] + " ");
         }
     }
 
-    public T removeFirst(){
-        if (nextfirst == items.length-1){
-            nextfirst = 0;
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
         }
-        else{
+
+        if (nextfirst == items.length - 1) {
+            nextfirst = 0;
+        } else {
             nextfirst = nextfirst + 1;
         }
         T result = items[nextfirst];
         items[nextfirst] = null;
         size -= 1;
 
-        if (size < items.length/4 && items.length>= 16){
-            T[] newitems =(T[]) new Object[items.length/2];
-            if (nextfirst > nextlast){
-                int back_length = items.length - nextfirst-1;
-                int back_position = newitems.length - back_length;
-                System.arraycopy(items,0, newitems, 0,nextlast);
-                System.arraycopy(items, nextfirst+1, newitems, back_position, back_length);
-                nextfirst = back_position-1;
-            }
-            else{
-                System.arraycopy(items, nextfirst+1, newitems,0, nextlast-nextfirst-1 );
-                nextlast = nextlast - nextfirst-1;
-                nextfirst = newitems.length-1;
+        if (size < items.length / 4 && items.length >= 16) {
+            T[] newitems = (T[]) new Object[items.length / 2];
+            if (nextfirst > nextlast) {
+                int backLength = items.length - nextfirst - 1;
+                int backPostion = newitems.length - backLength;
+                System.arraycopy(items, 0, newitems, 0, nextlast);
+                System.arraycopy(items, nextfirst + 1, newitems, backPostion, backLength);
+                nextfirst = backPostion - 1;
+            } else {
+                System.arraycopy(items, nextfirst + 1, newitems, 0, nextlast - nextfirst - 1);
+                nextlast = nextlast - nextfirst - 1;
+                nextfirst = newitems.length - 1;
 
             }
-            items=newitems;
+            items = newitems;
 
         }
         return result;
     }
 
     public T removeLast() {
-        if (nextlast == 0){
-            nextlast = items.length - 1;
+        if (size == 0) {
+            return null;
         }
-        else{
+
+        if (nextlast == 0) {
+            nextlast = items.length - 1;
+        } else {
             nextlast = nextlast - 1;
         }
-        T result=items[nextlast];
+        T result = items[nextlast];
         items[nextlast] = null;
         size -= 1;
 
-        if (size < items.length/4 && items.length>= 16){
-            T[] newitems =(T[]) new Object[items.length/2];
-            if (nextfirst > nextlast){
-                int back_length = items.length - nextfirst-1;
-                int back_position = newitems.length - back_length;
-                System.arraycopy(items,0, newitems, 0,nextlast);
-                System.arraycopy(items, nextfirst+1, newitems, back_position, back_length);
-                nextfirst = back_position-1;
+        if (size < items.length / 4 && items.length >= 16) {
+            T[] newitems = (T[]) new Object[items.length / 2];
+            if (nextfirst > nextlast) {
+                int backLength = items.length - nextfirst - 1;
+                int backPostion = newitems.length - backLength;
+                System.arraycopy(items, 0, newitems, 0, nextlast);
+                System.arraycopy(items, nextfirst + 1, newitems, backPostion, backLength);
+                nextfirst = backPostion - 1;
+            } else {
+                System.arraycopy(items, nextfirst + 1, newitems, 0, nextlast - nextfirst - 1);
+                nextlast = nextlast - nextfirst - 1;
+                nextfirst = newitems.length - 1;
             }
-            else{
-                System.arraycopy(items, nextfirst+1, newitems,0, nextlast-nextfirst-1 );
-                nextlast = nextlast - nextfirst-1;
-                nextfirst = newitems.length-1;
-
-            }
-            items=newitems;
+            items = newitems;
 
         }
 
         return result;
     }
 
-    public T get(int index){
-        if (size == 0 || index > size -1){
+    public T get(int index) {
+        if (size == 0 || index > size - 1) {
             return null;
         }
-        int back_length=items.length-nextfirst-1;
-        if (index < back_length){
-            return items[nextfirst+1+index];
-        }
-        else{
-            return items[index-back_length];
+        int backLength = items.length - nextfirst - 1;
+        if (index < backLength) {
+            return items[nextfirst + 1 + index];
+        } else {
+            return items[index - backLength];
         }
 
     }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,4 +1,3 @@
-import java.util.LinkedList;
 
 public class LinkedListDeque<T> {
     private ItemList sentinel;
@@ -9,11 +8,8 @@ public class LinkedListDeque<T> {
         ItemList next;
         ItemList before;
 
-        public ItemList() {
-            return;
-        }
 
-        public ItemList(T data, ItemList before, ItemList next) {
+        private ItemList(T data, ItemList before, ItemList next) {
             this.item = data;
             this.next = next;
             this.before = before;
@@ -21,33 +17,24 @@ public class LinkedListDeque<T> {
 
     }
 
-    public LinkedListDeque(T item) {
-
-        sentinel = new ItemList(null, null, null);
-        ItemList first = new ItemList(item, sentinel, sentinel);
-        sentinel.next = first;
-        sentinel.before = first;
-        size+=1;
-
-    }
     public LinkedListDeque() {
-        sentinel= new ItemList(null, null, null);
-        sentinel.before=sentinel;
-        sentinel.next=sentinel;
+        sentinel = new ItemList(null, null, null);
+        sentinel.before = sentinel;
+        sentinel.next = sentinel;
 
     }
 
-    public void addFirst(T item){
-        ItemList added = new ItemList(item, sentinel,sentinel.next);
-        sentinel.next.before=added;
-        sentinel.next=added;
-        size+=1;
+    public void addFirst(T item) {
+        ItemList added = new ItemList(item, sentinel, sentinel.next);
+        sentinel.next.before = added;
+        sentinel.next = added;
+        size += 1;
     }
 
-    public void addLast(T item){
+    public void addLast(T item) {
         ItemList added = new ItemList(item, sentinel.before, sentinel);
-        sentinel.before.next=added;
-        sentinel.before=added;
+        sentinel.before.next = added;
+        sentinel.before = added;
         size += 1;
     }
 
@@ -64,57 +51,57 @@ public class LinkedListDeque<T> {
     public void printDeque() {
         ItemList temp = this.sentinel;
         for (int i = 0; i < size; i++) {
-            System.out.print(temp.next.item+" ");
-            temp=temp.next;
+            System.out.print(temp.next.item + " ");
+            temp = temp.next;
         }
     }
 
-    public T removeFirst(){
-        if (size == 0){
+    public T removeFirst() {
+        if (size == 0) {
             return null;
         }
-        T removed=sentinel.next.item;
-        sentinel.next.next.before=sentinel;
-        sentinel.next=sentinel.next.next;
+        T removed = sentinel.next.item;
+        sentinel.next.next.before = sentinel;
+        sentinel.next = sentinel.next.next;
         size -= 1;
         return removed;
     }
 
-    public T removeLast(){
-        if (size == 0){
+    public T removeLast() {
+        if (size == 0) {
             return null;
         }
-        T removed=sentinel.before.item;
-        sentinel.before=sentinel.before.before;
-        sentinel.before.next=sentinel;
-        size -=1;
+        T removed = sentinel.before.item;
+        sentinel.before = sentinel.before.before;
+        sentinel.before.next = sentinel;
+        size -= 1;
         return removed;
     }
 
-    public T get(int index){
-        if (size == 0 || index > size-1) {
+    public T get(int index) {
+        if (size == 0 || index > size - 1) {
             return null;
         }
         int i = 0;
         ItemList temp = sentinel;
-        while(i < index+1) {
+        while (i < index + 1) {
             temp = temp.next;
             i++;
         }
-        T result =temp.item;
+        T result = temp.item;
         return result;
     }
 
-    public T getRecursive(int index){
-        T result=getRecursive(index, sentinel.next);
+    public T getRecursive(int index) {
+        T result = getRecursive(index, sentinel.next);
         return result;
     }
 
     private T getRecursive(int index, ItemList curr) {
-        if (index==0){
+        if (index == 0) {
             return curr.item;
         }
-        return getRecursive(index-1,curr.next);
+        return getRecursive(index - 1, curr.next);
     }
 
 
