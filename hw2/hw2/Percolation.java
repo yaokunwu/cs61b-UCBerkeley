d2b0e42b5e35b4fbb38b2d114c9b9a434cf1b623
Yaokun Wu <yaokun.wu.2017@gmail.com>
2020-06-09 23:17:37 -0500

    hw2 first attempt

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,105 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
 
+public class Percolation {
+    int opensize;
+    int[][] grids;
+    int[][] uf_2D;
+    WeightedQuickUnionUF uf;
+    WeightedQuickUnionUF uf2;
+    int vtInd;
+    int vbInd;
+    int n;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        this.n = n;
+        grids = new int[n][n];
+        uf_2D = new int[n][n];
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        uf2 = new WeightedQuickUnionUF(n * n + 2);
+        opensize = 0;
+        vtInd = n * n;
+        vbInd = n * n + 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                uf_2D[i][j] = i * n + j;
+                if (i == 0) {
+                    uf.union(uf_2D[i][j], vtInd);
+                    uf2.union(uf_2D[i][j], vtInd);
+                }
+                if (i == n - 1) {
+                    uf.union(uf_2D[i][j], vbInd);
+                }
+            }
+        }
+    }
+
+    private void connectaround(int row, int col) {
+        if (row >= 1 && isOpen(row - 1, col)) {
+            uf.union(uf_2D[row - 1][col], uf_2D[row][col]);
+            uf2.union(uf_2D[row - 1][col], uf_2D[row][col]);
+        }
+        if (row < n - 1 && isOpen(row + 1, col)) {
+            uf.union(uf_2D[row + 1][col], uf_2D[row][col]);
+            uf2.union(uf_2D[row + 1][col], uf_2D[row][col]);
+        }
+        if (col >= 1 && isOpen(row, col - 1)) {
+            uf.union(uf_2D[row][col - 1], uf_2D[row][col]);
+            uf2.union(uf_2D[row][col - 1], uf_2D[row][col]);
+        }
+        if (col < n - 1 && isOpen(row, col + 1)) {
+            uf.union(uf_2D[row][col + 1], uf_2D[row][col]);
+            uf2.union(uf_2D[row][col + 1], uf_2D[row][col]);
+        }
+    }
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (grids[row][col] == 0) {
+            grids[row][col] = 1;
+            opensize += 1;
+            connectaround(row, col);
+        }
+
+        if (!isOpen(row, col)) {
+            grids[row][col] = 1;
+            opensize += 1;
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (grids[row][col] == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        int ind = uf_2D[row][col];
+        return (isOpen(row, col) && uf2.connected(ind, vtInd));
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return opensize;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return uf.connected(vbInd, vtInd);
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+        // Percolation p = new Percolation(20);
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,59 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    private int T;
+    private double[] thre;
+
+    private double singleExp(int N, PercolationFactory pf) {
+        Percolation single = pf.make(N);
+        while (!single.percolates()) {
+            int row = StdRandom.uniform(N);
+            int col = StdRandom.uniform(N);
+            single.open(row, col);
+        }
+        return single.opensize / (double) (N * N);
+    }
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 | T <= 0) {
+            throw new IllegalArgumentException("invalid input!");
+        }
+
+        this.T = T;
+        double[] curr = new double[T];
+        for (int i = 0; i < T; i += 1) {
+            double result = singleExp(N, pf);
+            curr[i] = result;
+        }
+        this.thre = curr;
+    }   // perform T independent experiments on an N-by-N grid
+    public double mean() {
+        return StdStats.mean(thre);
+    }                                           // sample mean of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(thre);
+    }                                         // sample standard deviation of percolation threshold
+    public double confidenceLow() {
+        double aver = mean();
+        double sigm = stddev();
+        return aver - 1.96 * sigm / Math.sqrt(T);
+    }                                  // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        double aver = mean();
+        double sigm = stddev();
+        return aver + 1.96 * sigm / Math.sqrt(T);
+    } // high endpoint of 95% confidence interval
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+        PercolationFactory pf = new PercolationFactory();
+        PercolationStats simu = new PercolationStats(N, T, pf);
+        System.out.println("mean = " + simu.mean());
+        System.out.println("stddev = " + simu.stddev());
+        System.out.println("95% confidence interval = " + "[" + simu.confidenceLow() + " , "
+                + simu.confidenceHigh() + "]");
+    }
 }
